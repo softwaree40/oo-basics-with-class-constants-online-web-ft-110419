f0680116693e0a292e16ab13088ee54c50ae7415
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-11 00:34:26 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,17 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
+  attr_reader :brands
+    BRANDS = []
+   
   def initialize(brand)
-    @brand = brand
+    @brands = brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
 
 end
